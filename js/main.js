441c9b2e0732a22bd0fba6aec229b6556365d753
Елизавета <Elizaveta.Shmeleva@urfu.me>
2022-11-30 17:22:36 +0500

    Функции 2-ой раздел

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const checkMaxLength = (str, max = 100) => str.length <= max;
+
+const getRandomInt = (min, max) => {
+    if  (min < 0 || max < 0) {
+        return -1;
+    }
+
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
